dcb86a6c95f13cb4bb66a150f70740d280f802c4
GPhMorin <gphmorin@gmail.com>
2022-11-08 18:32:59 -0500

    Personnes.csv

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from scipy.io import wavfile
 
 def convert_to_csv():
     # Makes sure we scroll through all the relevant folders.
-    for person_folder in [folder for folder in os.listdir('./RawData/') if folder.startswith('Personne')]:
+    for person_folder in [folder for folder in os.listdir('./RawData/') if folder.startswith('Personne') and not folder.endswith('.csv')]:
         for wav_file in [file for file in os.listdir(f'./RawData/{person_folder}/wav/') if file.endswith('.wav')]:
 
             # Source code for the following: https://github.com/Lukious/wav-to-csv/blob/master/wav2csv.py
@@ -16,7 +16,37 @@ def convert_to_csv():
             recording = pd.DataFrame(data)
             recording.to_csv(f'./RawData/{person_folder}/wav/{wav_file.removesuffix(".wav")}.csv', index=False)
 
+
+def generate_group_csv():
+    persons = []
+
+    # Makes sure we scroll through all the relevant folders.
+    for person_folder in [folder for folder in os.listdir('./RawData/') if folder.startswith('Personne') and not folder.endswith('.csv')]:
+
+        with open(f'./RawData/{person_folder}/etc/README', 'r') as input:
+            # Remove all line returns
+            text = ' '.join(input.readlines())
+            # Transform the text into a list of tokens
+            text = text.split()
+            # Find the index of "Gender:" and get the gender from the next value
+            gender = text[text.index('Gender:') + 1]
+            # Find the index of "Language:" and get the language from the next value
+            language = text[text.index('Language:') + 1]
+
+        person = {
+            'ID': person_folder,
+            'Gender': gender,
+            'Language': language
+        }
+
+        persons.append(person)
+
+    persons = pd.DataFrame(persons)
+    persons.to_csv(f'./RawData/Personnes.csv', index=False)
+
 if __name__ == '__main__':
     convert_to_csv()
+
+    generate_group_csv()
 
     print('We are the champions 🎶')
