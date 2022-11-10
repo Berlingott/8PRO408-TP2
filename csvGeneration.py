9a07d6e5b5ec1511d6c0e3a15e03862a971eabde
GPhMorin <gphmorin@gmail.com>
2022-11-09 19:08:55 -0500

    Create CleanData

---
+++ csvGeneration.py
@@ -0,0 +1,171 @@
+import concurrent.futures
+from featureExtraction import extract_features
+import multiprocessing
+import os
+import pandas as pd
+import pickle
+from scipy.io import wavfile
+import shutil
+import time
+
+sentences = ['a0003', 'a0004', 'a0005', 'a0006']
+time_window_length = 500
+overlap_length = 400
+non_overlap_length = time_window_length - overlap_length
+x_data = []
+y_data = []
+label_data = []
+total_time = 0
+
+def convert_to_csv():
+    print('Converting WAV files to CSV.')
+    # Makes sure we scroll through all the relevant folders: starts with "Personne" and is actually a folder.
+    for person_folder in [file for file in os.listdir('./RawData/') if os.path.isdir(f'./RawData/{file}')]:
+        # Browse the WAV files
+        for sentence in sentences:
+
+            # Source code for the following: https://github.com/Lukious/wav-to-csv/blob/master/wav2csv.py
+            # Used to convert a WAV file into a CSV file.
+
+            _, data = wavfile.read(f'./RawData/{person_folder}/wav/{sentence}.wav')  # The first rejected element is the rate, which is always 16kHz
+
+            recording = pd.DataFrame(data)
+            recording.columns = ['frequency']
+            recording.to_csv(f'./RawData/{person_folder}/wav/{sentence}.csv', index=False)
+
+
+def one_csv_per_person():
+    print('Merging the CSV files into one file per person.')
+    # Scrolls through each person
+    for person_folder in [file for file in os.listdir('./RawData/') if os.path.isdir(f'./RawData/{file}')]:
+        # Browse the sentence CSV files
+        final = pd.DataFrame()
+        for sentence in sentences:
+            column = pd.read_csv(f'./RawData/{person_folder}/wav/{sentence}.csv')
+            column = column.squeeze(axis=0)
+            final[sentence] = column
+        final.to_csv(f'./RawData/{person_folder}/person.csv', index=False)
+
+
+
+
+def generate_group_csv():
+    print('Generating an overview file of the persons\' categorical values.')
+    persons = list()
+
+    # Makes sure we scroll through all the relevant folders: starts with "Personne" and is actually a folder.
+    for person_folder in [file for file in os.listdir('./RawData/') if os.path.isdir(f'./RawData/{file}')]:
+
+        with open(f'./RawData/{person_folder}/etc/README', 'r') as input:
+            # Remove all line returns
+            text = ' '.join(input.readlines())
+            # Transform the text into a list of tokens
+            text = text.split()
+            # Find the index of "Gender:" and get the gender from the next value
+            gender = text[text.index('Gender:') + 1]
+            # Find the index of "Language:" and get the language from the next value
+            try:
+                language_tmp = text[text.index('Language:') + 1]
+            except ValueError:
+                language_tmp = 'Unknown'
+            language = language_tmp
+
+        person = {
+            'ID': person_folder,
+            'Gender': gender,
+            'Language': language
+        }
+
+        persons.append(person)
+
+    persons = pd.DataFrame(persons)
+    persons.to_csv('./RawData/persons.csv', index=False)
+
+
+def clean_data():
+    print('Creating a clean folder.')
+    persons = [person for person in os.listdir('./RawData/') if os.path.isdir(f'./RawData/{person}')]
+    for person in persons:
+        os.makedirs(os.path.dirname(f'./CleanData/{person}/person.csv'), exist_ok=True)
+        shutil.copy(f'./RawData/{person}/person.csv', f'./CleanData/{person}/person.csv')
+        for sentence in sentences:
+            shutil.copy(f'./RawData/{person}/wav/{sentence}.csv', f'./CleanData/{person}/{sentence}.csv')
+
+
+def extract_data(person, sentence):
+    print('Extracting the data from the CSV files.')
+
+    raw_data = pd.read_csv(f'./RawData/{person}/wav/{sentence.removesuffix(".wav")}.csv')
+
+    column_names = raw_data.columns
+    data_to_use = raw_data.values
+
+    x_data_temp = list()
+    y_data_temp = list()
+
+    print(person, sentence)
+    for i in range(0, data_to_use.shape[0] - time_window_length, non_overlap_length):
+        x = data_to_use[i: i + time_window_length]
+        start_time = time.time()
+        # Extract features
+        feature_vector, features = extract_features(x)
+        end_time = time.time()
+        global total_time
+        total_time = total_time + (end_time - start_time)
+
+
+        x_data_temp.append(feature_vector)
+        y_data_temp.append(sentence)
+        
+    return column_names, features, x_data_temp, y_data_temp
+
+
+def generate_dataset():
+    # Initialization
+    persons = [person for person in os.listdir('./RawData/') if os.path.isdir(f'./RawData/{person}')]
+    
+    arguments = [tuple((person, sentence)) for person in persons for sentence in sentences]
+
+    # for person, sentence in arguments:
+    #     column_names, features = extract_data(person, sentence)
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=multiprocessing.cpu_count()) as e:
+        futures = [e.submit(extract_data, person, sentence) for person, sentence in arguments]
+        for future in concurrent.futures.as_completed(futures):
+            column_names, features, x_data_temp, y_data_temp = future.result()
+            global x_data
+            x_data.append(x_data_temp)
+            global y_data
+            y_data.append(y_data_temp)
+
+    new_column_names = list()
+
+    for feature in features:
+        for column_name in column_names:
+            new_column_names.append(f'{feature}_{column_name}')
+
+
+    x_data = pd.DataFrame(x_data, columns=new_column_names)
+    # x_data.insert(0, 'Label', label_data)  # If we can to retrieve the file easily
+    x_data.to_csv('x_data.csv', index=False)
+
+    print(f'The total time to extract all features is: {total_time}.')
+    print(f'The size of the dataset is: {x_data.shape[0]} by {x_data.shape[1]} (instances x features).')
+    print(f'The number of labels is: {len(y_data)}.')
+
+    # Save the dataset
+    with open('dataset.pickle', 'wb') as output:
+        pickle.dump([x_data, y_data], output)
+
+if __name__ == '__main__':
+    convert_to_csv()
+
+    one_csv_per_person()
+
+    generate_group_csv()
+
+    clean_data()
+
+    # generate_dataset()
+
+    print('We are the champions 🎶')
--- main.py
+++ main.py
@@ -1,153 +1,103 @@
-import concurrent.futures
-from featureExtraction import extract_features
-import multiprocessing
 import os
+
 import pandas as pd
-import pickle
-from scipy.io import wavfile
+
 import time
 
-sentences = ['a0003', 'a0004', 'a0005', 'a0006']
-time_window_length = 500
-overlap_length = 400
+from featureExtraction import extract_features
+import pickle
+
+# USER PARAMETERS
+
+raw_dataset_pathname = "C:\\8PRO408 - Automne 2022\\Live coding 2\\Basic Daily Activity Dataset"
+
+object_list = ["Assiette", "Cafetiere", "Casserole", "Couteau", "Cuillere", "Fouet",
+               "Fourchette", "Louche", "MainAVide", "Poele", "PressoirePatates",
+               "Rouleau", "Tasse", "Verre"]
+
+time_window_length = 15
+overlap_length = 10
+
+extracted_dataset_pathname = "C:\\8PRO408 - Automne 2022\\Live coding 2\\Basic Daily Activity Dataset"
+
+new_dataset_name = "New_dataset_15_10"
+
+
+# INITIALIZATION
+
+folder_list = os.listdir(raw_dataset_pathname)
+folder_number = len(folder_list)
+
 non_overlap_length = time_window_length - overlap_length
+
+
+# CREATE THE DATASET
+
 x_data = []
 y_data = []
-label_data = []
+
 total_time = 0
 
-def convert_to_csv():
-    # Makes sure we scroll through all the relevant folders: starts with "Personne" and is actually a folder.
-    for person_folder in [file for file in os.listdir('./RawData/') if os.path.isdir(f'./RawData/{file}')]:
-        # Browse the WAV files
-        for sentence in sentences:
+for folder_name in folder_list:
 
-            # Source code for the following: https://github.com/Lukious/wav-to-csv/blob/master/wav2csv.py
-            # Used to convert a WAV file into a CSV file.
+    print("We are processing: " + folder_name)
 
-            _, data = wavfile.read(f'./RawData/{person_folder}/wav/{sentence}.wav')  # The first rejected element is the rate, which is always 16kHz
+    tmp_pathname = os.path.join(raw_dataset_pathname, folder_name)
 
-            recording = pd.DataFrame(data)
-            recording.to_csv(f'./RawData/{person_folder}/wav/{sentence}.csv', index=False)
+    file_list = os.listdir(tmp_pathname)
 
+    for file_name in file_list:
 
-def one_csv_per_person():
-    # Scrolls through each person
-    for person_folder in [file for file in os.listdir('./RawData/') if os.path.isdir(f'./RawData/{file}')]:
-        # Browse the sentence CSV files
-        final = pd.DataFrame()
-        for sentence in sentences:
-            column = pd.read_csv(f'./RawData/{person_folder}/wav/{sentence}.csv')
-            column = column.squeeze(axis=0)
-            final[sentence] = column
-        final.to_csv(f'./RawData/{person_folder}/person.csv', index=False)
+        for object in object_list:
 
+            if object in file_name:
 
+                print("     - " + file_name)
 
+                raw_data = pd.read_csv(os.path.join(tmp_pathname, file_name),
+                                       sep=",", skiprows=0, engine="python")
 
-def generate_group_csv():
-    persons = list()
+                column_names = raw_data.columns
 
-    # Makes sure we scroll through all the relevant folders: starts with "Personne" and is actually a folder.
-    for person_folder in [file for file in os.listdir('./RawData/') if os.path.isdir(f'./RawData/{file}')]:
+                data_to_use = raw_data.values
 
-        with open(f'./RawData/{person_folder}/etc/README', 'r') as input:
-            # Remove all line returns
-            text = ' '.join(input.readlines())
-            # Transform the text into a list of tokens
-            text = text.split()
-            # Find the index of "Gender:" and get the gender from the next value
-            gender = text[text.index('Gender:') + 1]
-            # Find the index of "Language:" and get the language from the next value
-            try:
-                language_tmp = text[text.index('Language:') + 1]
-            except ValueError:
-                language_tmp = 'Unknown'
-            language = language_tmp
+                for k in range(0, data_to_use.shape[0] - time_window_length, non_overlap_length):
 
-        person = {
-            'ID': person_folder,
-            'Gender': gender,
-            'Language': language
-        }
+                    x = data_to_use[k: k+time_window_length]
 
-        persons.append(person)
-
-    persons = pd.DataFrame(persons)
-    persons.to_csv('./RawData/persons.csv', index=False)
+                    start_time = time.time()
+                    # Extract features
+                    vector_features, feature_list = extract_features(x)
+                    end_time = time.time()
 
 
-def extract_data(person, sentence):
-        raw_data = pd.read_csv(f'./RawData/{person}/wav/{sentence.removesuffix(".wav")}.csv')
+                    total_time = total_time + (end_time - start_time)
 
-        column_names = raw_data.columns
-        data_to_use = raw_data.values
+                    x_data.append(vector_features)
+                    y_data.append(object)
 
-        x_data_temp = list()
-        y_data_temp = list()
+new_column_names = []
 
-        print(person, sentence)
-        for i in range(0, data_to_use.shape[0] - time_window_length, non_overlap_length):
-            x = data_to_use[i: i + time_window_length]
-            start_time = time.time()
-            # Extract features
-            feature_vector, features = extract_features(x)
-            end_time = time.time()
-            global total_time
-            total_time = total_time + (end_time - start_time)
+for feature in feature_list:
 
+    for column_name in column_names:
 
-            x_data_temp.append(feature_vector)
-            y_data_temp.append(sentence)
-            
-        return column_names, features, x_data_temp, y_data_temp
+        new_column_names.append(feature + "_" + column_name)
+
+x_data = pd.DataFrame(x_data, columns=new_column_names)
+
+print("\n\nThe total time to extract all features is: " + str(total_time))
+
+print("The size of the dataset is: " + str(x_data.shape[0]) + "x" + str(x_data.shape[1]) + "(instances x features)")
+print("The number of labels is: " + str(len(y_data)))
+
+# SAVE THE DATASET
+with open(os.path.join(extracted_dataset_pathname, new_dataset_name + ".pickle"), "wb") as f:
+    pickle.dump([x_data, y_data], f)
 
 
 
-def generate_dataset():
-    # Initialization
-    persons = [person for person in os.listdir('./RawData/') if os.path.isdir(f'./RawData/{person}')]
-    
-    arguments = [tuple((person, sentence)) for person in persons for sentence in sentences]
-
-    # for person, sentence in arguments:
-    #     column_names, features = extract_data(person, sentence)
-
-    with concurrent.futures.ThreadPoolExecutor(max_workers=multiprocessing.cpu_count()) as e:
-        futures = [e.submit(extract_data, person, sentence) for person, sentence in arguments]
-        for future in concurrent.futures.as_completed(futures):
-            column_names, features, x_data_temp, y_data_temp = future.result()
-            global x_data
-            x_data.append(x_data_temp)
-            global y_data
-            y_data.append(y_data_temp)
-
-    new_column_names = list()
-
-    for feature in features:
-        for column_name in column_names:
-            new_column_names.append(f'{feature}_{column_name}')
 
 
-    x_data = pd.DataFrame(x_data, columns=new_column_names)
-    # x_data.insert(0, 'Label', label_data)  # If we can to retrieve the file easily
-    x_data.to_csv('x_data.csv', index=False)
 
-    print(f'The total time to extract all features is: {total_time}.')
-    print(f'The size of the dataset is: {x_data.shape[0]} by {x_data.shape[1]} (instances x features).')
-    print(f'The number of labels is: {len(y_data)}.')
 
-    # Save the dataset
-    with open('dataset.pickle', 'wb') as output:
-        pickle.dump([x_data, y_data], output)
-
-if __name__ == '__main__':
-    # convert_to_csv()
-
-    # one_csv_per_person()
-
-    # generate_group_csv()
-
-    # generate_dataset()
-
-    print('We are the champions 🎶')
